2f91a06cbc53d37a9983f9c8399aac48d024835a
Guilherme-Soares777 <guilhermesoaresavilars@gmail.com>
2022-09-14 14:00:22 -0300

    Praticando Exercícios de laços de repetição

---
+++ main.js
@@ -0,0 +1,22 @@
+/*EXERCICIO 7- Estrutura de Repetição
+const m = 6
+let n = 8
+let i = 2
+for (i = m; i <= n; i++) {
+    console.log(i)
+} */
+
+
+
+let m = 3;
+let n = 14;
+let i = 2;
+
+for (j = m; j <= n; j += i) {
+    console.log(j);
+}
+
+
+
+
+
